b5bbf5c11f9f8c543083c9bfa51269017d1e546a
rahulgidde <rahulgidde1212@gmail.com>
2020-02-15 19:02:53 +0530

    ADDED:Store Result In Dictionary For Every Computation

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -16,3 +16,13 @@ result3=`echo "scale=2; $input3 + $input1 / $input2" | bc`
 
 #COMPUTING FOURTH EXPRESSTION
 result4=`echo "scale=2; $input1 % $input2 + $input3" | bc`
+
+#DECLARE DICTIONARY
+declare -A Dictionary
+
+#STORE COMPUTATION RESULTS IN DICTIONARY
+Dictionary[1]=$result1
+Dictionary[2]=$result2
+Dictionary[3]=$result3
+Dictionary[4]=$result4
+
