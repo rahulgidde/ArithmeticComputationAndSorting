3a7b031d9cac7e5cf3fa550edaa79cfe72c119f0
rahulgidde <rahulgidde1212@gmail.com>
2020-02-15 18:47:47 +0530

    ADDED:Computing Fourth Expresstion

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -14,3 +14,5 @@ result2=`echo "scale=2; $input1 * $input2 + $input3" | bc`
 #COMPUTING THIRD EXPRESSTION
 result3=`echo "scale=2; $input3 + $input1 / $input2" | bc`
 
+#COMPUTING FOURTH EXPRESSTION
+result4=`echo "scale=2; $input1 % $input2 + $input3" | bc`
