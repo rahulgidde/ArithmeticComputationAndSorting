b57da5ba8920b59970f235b2bb5fa16e16518862
rahulgidde <rahulgidde1212@gmail.com>
2020-02-15 18:37:34 +0530

    ADDED:Computing First Expresstion

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -5,3 +5,5 @@ read -p  "Enter Number: " input1
 read -p  "Enter Number: " input2
 read -p  "Enter Number: " input3
 
+#COMPUTING FIRST EXPRESSTION
+result1=`echo "scale=2; $input1 + $input2 * $input3" | bc`
