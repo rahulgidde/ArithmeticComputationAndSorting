c5895563a7a888fe2e080301f7aca8543794dc44
rahulgidde <rahulgidde1212@gmail.com>
2020-02-15 18:43:17 +0530

    ADDED:Computing Third Expresstion

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -11,3 +11,6 @@ result1=`echo "scale=2; $input1 + $input2 * $input3" | bc`
 #COMPUTING SECOND EXPRESSTION
 result2=`echo "scale=2; $input1 * $input2 + $input3" | bc`
 
+#COMPUTING THIRD EXPRESSTION
+result3=`echo "scale=2; $input3 + $input1 / $input2" | bc`
+
