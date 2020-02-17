8e8176de526dd702021292472c41f6723418ad15
rahulgidde <rahulgidde1212@gmail.com>
2020-02-17 11:39:45 +0530

    ADDED:Sorting Array in Decending Order

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -35,3 +35,17 @@ do
 	Array[$index1]=${Dictionary[$index1]}
 done
 
+#SORTING THE COMPUTATION RESULT IN DECENDING ORDER
+for (( index=0; index<${#Array[@]}; index++ ))
+do
+	for (( index1=$index+1; index1<${#Array[@]}; index1++ ))
+	do
+		if (( $(echo "${Array[index]} < ${Array[index1]}" |bc -l) ))
+		then
+			temp=${Array[index]}
+			Array[index]=${Array[index1]}
+			Array[index1]=$temp
+		fi
+	done
+done
+
